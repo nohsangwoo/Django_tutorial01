16f88677e3806066a9e11dce5eb334d37c0a4274
nohsangwoo <nsgr12@naver.com>
2022-06-02 15:52:48 +0900

    finish that tutorial3

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'polls'
+
+
 urlpatterns = [
-    path('', views.index, name='index'),
     path('test/', views.test, name='test'),
+    path('', views.index, name='index'),
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
