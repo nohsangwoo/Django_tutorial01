39c6bd69c123fda44c969e0dda119b4124b4e12c
nohsangwoo <nsgr12@naver.com>
2022-06-02 15:35:45 +0900

    error handling

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # create Django Tutorial project1
 
+# project 실행
+
+- python manage.py runserver
+
 # route 관리
+
+# Django는 jinja2 template language로 html을 표현한다
+
+# render를 이용하여 보다 편하게 html template를 렌더링 할 수 있다.
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,25 @@
+from django.template import loader
 from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from polls.models import Question
+from django.shortcuts import get_object_or_404, render
 
 
 def test(request):
     return HttpResponse("It's a test.")
 
 
+def index(request):
+    # [:5] : 최근 5개를 뽑아와라
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # 에러 핸들링(DB에 없는 값을 가져올때 에러핸들링)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
